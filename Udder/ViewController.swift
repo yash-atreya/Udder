b4d5126ea9d6f0e342559afe0efc9b74629c77c7
Yash Atreya <yashatreya.ya@icloud.com>
2018-12-08 19:20:01 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Yash Atreya on 08/12/18.
+//  Copyright © 2018 Yash Atreya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
